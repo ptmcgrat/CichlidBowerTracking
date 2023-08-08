7c222f21f58e0d6681374b2d60b5fb961d04da1d
Breanna Shi <74272793+breannashi@users.noreply.github.com>
2023-08-08 15:02:25 -0400

    Update add_fish_sex.py

--- cichlid_bower_tracking/unit_scripts/add_fish_sex.py
+++ cichlid_bower_tracking/unit_scripts/add_fish_sex.py
@@ -1,7 +1,7 @@
 import argparse, subprocess
 from cichlid_bower_tracking.data_preparers.project_preparer import ProjectPreparer as PP
 
-parser = argparse.ArgumentParser(usage = 'This script will use YOLOV5 to detect fish and SORT to link tracks')
+parser = argparse.ArgumentParser(usage = 'This script will use Pytorch Fish Sex Detection model')
 parser.add_argument('ProjectID', type = str, help = 'Manually identify the project you want to analyze')
 parser.add_argument('AnalysisID', type = str, help = 'Manually identify the project you want to analyze')
 parser.add_argument('--VideoIndex', type = int, help = 'Index of video that should be analyzed')
@@ -9,5 +9,5 @@ parser.add_argument('--VideoIndex', type = int, help = 'Index of video that shou
 args = parser.parse_args()
 
 pp_obj = PP(projectID = args.ProjectID, analysisID = args.AnalysisID)
-pp_obj.runTrackFishAnalysis(args.VideoIndex)
+pp_obj.runMLFishDetection(args.VideoIndex)
 
