96d1c2e80712e07316e8e46b13cb9203670ea327
breannashi <shi00231@umn.edu>
2023-08-13 23:10:57 -0400

    test

--- cichlid_bower_tracking/runsexAnalysis.py
+++ cichlid_bower_tracking/runsexAnalysis.py
@@ -1,6 +1,6 @@
 import argparse, subprocess, pdb, datetime, os, sys
 import pandas as pd
-sys.path.append('/data/home/bshi42/CichlidBowerTracking/cichlid_bower_tracking') 
+sys.path.append('/data/home/bshi42/CichlidBowerTracking/') 
 print(sys.path)
 
 from cichlid_bower_tracking.helper_modules.file_manager import FileManager as FM
