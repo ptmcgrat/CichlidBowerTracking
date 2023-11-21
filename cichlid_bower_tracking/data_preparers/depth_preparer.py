70bc3ccf01ec8a6e1cf1ee37e73673bed3a5ea94
McGrath <pmcgrath7@ipsec-10-2-226-62.vpn.gatech.edu>
2023-11-20 22:02:25 -0500

    Fix Fastq files

--- cichlid_bower_tracking/data_preparers/depth_preparer.py
+++ cichlid_bower_tracking/data_preparers/depth_preparer.py
@@ -197,7 +197,6 @@ class DepthPreparer:
 
             start_index = project_info.loc['Trial_' + str(i)].first_index
             last_index = project_info.loc['Trial_' + str(i)].last_index
-            pdb.set_trace()
             reset_index = project_info.loc['Trial_' + str(i) + '_Reset'].last_index
             #totalChangeData = vars(self.da_obj.returnVolumeSummary(self.lp.frames[start_index].time, self.lp.frames[last_index].time))
 
