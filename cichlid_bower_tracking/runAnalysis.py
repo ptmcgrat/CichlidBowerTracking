d8eef86c9853ac6efcb90ad6c74b6ef7af7b7126
McGrath <pmcgrath7@ipsec-10-2-226-62.vpn.gatech.edu>
2023-11-22 08:27:24 -0500

    Fix Fastq files

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -25,7 +25,7 @@ p_flag = False
 for subjectID, row in fm_obj.s_dt.iterrows():
 	for projectID in row.ProjectIDs.split(',,'):
 
-		while not p_flag and projectID != 'Running: MC_s9_tr3_BowerBuilding':
+		if not p_flag and projectID != 'Running: MC_s9_tr3_BowerBuilding':
 			continue
 		p_flag = True		
 		print('Running: ' + projectID + ' ' + str(datetime.datetime.now()), flush = True)
