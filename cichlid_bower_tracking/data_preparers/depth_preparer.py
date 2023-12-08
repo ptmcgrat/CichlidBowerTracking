5ee4046489fb3ce6b49e821695c40fd3c2c12fdd
Patrick McGrath <pmcgrath7@ipsec-10-2-225-146.vpn.gatech.edu>
2023-12-08 14:55:45 -0500

    Changed to handle current AnalysisStates file

--- cichlid_bower_tracking/data_preparers/depth_preparer.py
+++ cichlid_bower_tracking/data_preparers/depth_preparer.py
@@ -236,6 +236,9 @@ class DepthPreparer:
                     current_grid_idx += 1
                     midGrid = gridspec.GridSpecFromSubplotSpec(3, num_days + 1, subplot_spec=gridDaily[current_grid_idx])
 
+                if day >= 7:
+                    pdb.set_trace()
+
                 current_axs = [figDaily.add_subplot(midGrid[n, (num_days - j % num_days) - 1]) for n in [0, 1, 2]]
                 current_axs[0].imshow(self.da_obj.returnHeightChange(self.lp.frames[day_info.iloc[-1].day_start].time, self.lp.frames[day_stop + 1].time, cropped=True), vmin=-v, vmax=v)
                 bowerVolume = self.da_obj.returnVolumeSummary(self.lp.frames[day_start-1].time,self.lp.frames[day_stop+1].time).depthBowerVolume
