4000cd6d591df1f95024f9274bb1a371e2b2b3cd
Patrick McGrath <pmcgrath7@ipsec-10-2-227-63.vpn.gatech.edu>
2025-06-20 18:07:53 -0400

    Small updates

--- cichlid_bower_tracking/unit_scripts/drive_updater.py
+++ cichlid_bower_tracking/unit_scripts/drive_updater.py
@@ -173,7 +173,8 @@ class DriveUpdater:
             else:
                 trial_frames = [x for x in daylightFrames if x.time > self.lp.tankresetstop[j-1] and x.time < self.lp.tankresetstart[j]]
             days = {}
-            [days[x.time.day] for x in trial_frames]
+            for x in trial_frames:
+                days[x.time.day] = 1
             pdb.set_trace()
             img_1 = img.imread(self.projectDirectory + trial_frames[0].pic_file)
             img_2 = img.imread(self.projectDirectory + trial_frames[-1].pic_file)
