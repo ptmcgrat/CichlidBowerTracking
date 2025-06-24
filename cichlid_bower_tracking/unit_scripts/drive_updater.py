9a38b15e5af863373e572d632ac2777eea6754c2
Patrick McGrath <pmcgrath7@ipsec-10-2-97-123.vpn.gatech.edu>
2025-06-24 11:54:12 -0400

    Small updates

--- cichlid_bower_tracking/unit_scripts/drive_updater.py
+++ cichlid_bower_tracking/unit_scripts/drive_updater.py
@@ -124,27 +124,24 @@ class DriveUpdater:
 
         for i in range(5):
             axes.append(fig.add_subplot(num_rows, 5, i+1))
-        axes[0].set_title('Depth RGB')
-        axes[1].set_title('PiCamera RGB')
+        axes[0].set_title('Latest Depth RGB')
+        axes[1].set_title('Latest PiCamera RGB')
         axes[2].set_title('First Depth')
         axes[3].set_title('Current Depth')
         axes[4].set_title('Last day change')
 
-        print('Starting' + str(datetime.datetime.now()))
+        #print('Starting' + str(datetime.datetime.now()))
         img_1 = img.imread(self.projectDirectory + self.lp.frames[-1].pic_file)
         img_2 = img.imread(self.projectDirectory + self.lp.movies[-1].pic_file)
-        print('Current images read' + str(datetime.datetime.now()))
+        #print('Current images read' + str(datetime.datetime.now()))
         depth_first = self._filterPixels(np.load(self.projectDirectory + daylightFrames[0].npy_file))
-        print('Depth read' + str(datetime.datetime.now()))
-        
+        #print('Depth read' + str(datetime.datetime.now()))
         depth_last = self._filterPixels(np.load(self.projectDirectory + self.lp.frames[-1].npy_file))
-        print('Depth 2 read' + str(datetime.datetime.now()))
-        
+        #print('Depth 2 read' + str(datetime.datetime.now()))
         depth_dayend = self._filterPixels(np.load(self.projectDirectory + daylightFrames[-1].npy_file))
-        print('Depth 3 read' + str(datetime.datetime.now()))
-        
+        #print('Depth 3 read' + str(datetime.datetime.now()))
         depth_daystart = self._filterPixels(np.load(self.projectDirectory + daylightFrames_day[0].npy_file))
-        print('Depth 4 read' + str(datetime.datetime.now()))
+        #print('Depth 4 read' + str(datetime.datetime.now()))
         
         median_height = np.nanmedian(depth_first)
 
@@ -175,7 +172,7 @@ class DriveUpdater:
             days = {}
             for x in trial_frames:
                 days[x.time.day] = 1
-            
+            print(str(j) + ':' + str(days)) 
 
             img_1 = img.imread(self.projectDirectory + trial_frames[0].pic_file)
             img_2 = img.imread(self.projectDirectory + trial_frames[-1].pic_file)
@@ -189,7 +186,7 @@ class DriveUpdater:
             threshold = 1
             bower_mask = np.where(total_depth_change < (-1*threshold), -1, np.where(total_depth_change > threshold,1,0))
             for current_day in days:
-                print('Volume calculated' + str(datetime.datetime.now()))
+                #print('Volume calculated' + str(datetime.datetime.now()))
                 day_data = [x for x in trial_frames if x.time.day == current_day]
                 day_start = self._filterPixels(np.load(self.projectDirectory + day_data[0].npy_file))
                 day_stop = self._filterPixels(np.load(self.projectDirectory + day_data[-1].npy_file))
