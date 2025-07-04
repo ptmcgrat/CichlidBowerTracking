abe7d78987c6ebc68538fc7ec35bd6dc0ad699f7
Patrick McGrath <pmcgrath7@ipsec-10-2-97-123.vpn.gatech.edu>
2025-07-04 09:36:54 -0400

    Small updates

--- cichlid_bower_tracking/helper_modules/log_parser.py
+++ cichlid_bower_tracking/helper_modules/log_parser.py
@@ -236,9 +236,11 @@ class Trial:
         self.startTime = start_time
         self.stopTime = stop_time
         self.resetTime = reset_time
-        self.frames = [x for x in all_frames if x.time > start_time and x.time < stop_time]
-        self.daylight_frames = [x for x in self.frames if x.lof == True]
-        pdb.set_trace()
+        try:
+            self.frames = [x for x in all_frames if x.time > start_time and x.time < stop_time]
+            self.daylight_frames = [x for x in self.frames if x.lof == True]
+        except IndexError:
+            pdb.set_trace()
         if reset_time is not None:
             self.reset_frame = [x for x in self.daylight_frames if x.time > reset_time][0]
         try:
