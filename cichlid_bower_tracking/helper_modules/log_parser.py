ffdf58574f0f9eca11a5e6a700a4d943a5393744
Patrick McGrath <pmcgrath7@ipsec-10-2-97-123.vpn.gatech.edu>
2025-07-04 10:03:11 -0400

    Small updates

--- cichlid_bower_tracking/helper_modules/log_parser.py
+++ cichlid_bower_tracking/helper_modules/log_parser.py
@@ -80,6 +80,7 @@ class LogParser:
                     self.master_stop = self._ret_data(line, ['Time'])[0]
 
         self.frames.sort(key = lambda x: x.time)
+        pdb.set_trace()
         if self.running:
             self.movies[-1].endTime = self.frames[-1].time
         # Create trials
