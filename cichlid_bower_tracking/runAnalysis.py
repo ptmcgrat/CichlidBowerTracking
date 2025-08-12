4da880c4e41815ae213f8cb456646d1fad00629a
Patrick McGrath <pmcgrath7@lawn-128-61-3-106.lawn.gatech.edu>
2025-08-12 11:35:00 -0400

    Small updates

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -159,6 +159,7 @@ elif args.AnalysisType == 'Cluster':
 		else:
 			videoIndices = range(range(len(fm_obj.lp.movies)))
 
+		pdb.set_trace()
 		already_run = [] if ':' not in row.Cluster else row.Cluster.split(': ')[1].split(',')
 		videoIndices = [x for x in videoIndices if x not in already_run]
 		print('Running: ' + ','.join([str(x) for x in videoIndices]), flush = True)
