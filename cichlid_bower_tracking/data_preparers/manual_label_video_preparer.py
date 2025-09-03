99d332a588826b33e17f76ce5e020903cc4aaea3
Patrick McGrath <pmcgrath7@ipsec-10-2-97-122.vpn.gatech.edu>
2025-09-03 09:08:27 -0400

    Small updates

--- cichlid_bower_tracking/data_preparers/manual_label_video_preparer.py
+++ cichlid_bower_tracking/data_preparers/manual_label_video_preparer.py
@@ -168,13 +168,15 @@ class ManualLabelVideoPreparer():
 		for videoIndex in self.videoIndices:
 			videoObj = self.fileManager.returnVideoObject(videoIndex)
 			frames += [videoObj.localManualLabelFramesDir + x for x in os.listdir(videoObj.localManualLabelFramesDir)]
+		
+		random.shuffle(frames) # Shuffle the clips so that it's a random sample
 
 		index = 0
 		while index < len(frames): # We use a while loop so we can reannotate a clip if a mistake is made
 			f = frames[index] # Get current clip
 			frame_name = self.fileManager.projectID + '__' + f.split('/')[-1]
 			if frame_name in labeled_dt.ClipName:
-				print('Skipping ' + clip_name + ' since it is already labeled', file = sys.stderr)
+				print('Skipping ' + frame_name + ' since it is already labeled', file = sys.stderr)
 				continue
 
 			labeled_dt.loc[len(labeled_dt)] = [self.fileManager.analysisID, self.fileManager.projectID, frame_name, '', ''] # Create new annotation
