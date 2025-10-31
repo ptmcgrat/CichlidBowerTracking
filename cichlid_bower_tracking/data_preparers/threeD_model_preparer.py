7636b58a6978ca8fa61be53c29d92ead2e14393a
Patrick McGrath <pmcgrath7@ipsec-10-2-98-80.vpn.gatech.edu>
2025-10-31 15:42:57 -0400

    Small updates

--- cichlid_bower_tracking/data_preparers/threeD_model_preparer.py
+++ cichlid_bower_tracking/data_preparers/threeD_model_preparer.py
@@ -45,7 +45,7 @@ class ThreeDModelPreparer():
 		command.extend(['--Input_videos_directory', self.fileManager.localLabeledClipsDir])
 		command.extend(['--ML_labels', self.fileManager.localVideoProjectsFile])
 		command.extend(['--Results_directory', self.fileManager.local3DModelTempDir])
-		command.extend(['--Log', self.localModelCondaVersionsFile])
+		command.extend(['--Log', self.fileManager.localModelCondaVersionsFile])
 		
 		command.extend(['--gpu', str(0)])
 		
