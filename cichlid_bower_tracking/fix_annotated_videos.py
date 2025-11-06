3171fab9a1fd879667e4bf5d708ae1c345b4f7ae
Patrick McGrath <pmcgrath7@ipsec-10-2-98-80.vpn.gatech.edu>
2025-11-06 17:00:07 -0500

    Small updates

--- cichlid_bower_tracking/fix_annotated_videos.py
+++ cichlid_bower_tracking/fix_annotated_videos.py
@@ -7,8 +7,9 @@ fm_obj.downloadData(fm_obj.localLabeledClipsDir, tarred_subdirs = True)
 fm_obj.downloadData(fm_obj.localLabeledClipsFile)
 
 dt = pd.read_csv(fm_obj.localLabeledClipsFile)
+dt['VideoExists'] = True
 
 for index,row in dt.iterrows():
-	video_file_path = os.path.join(fm_obj.localLabeledClipsDir,row.ClipName)
+	video_file_path = os.path.join(fm_obj.localLabeledClipsDir,row.ClipName + '.mp4')
 	if not os.path.exists(video_file_path):
 		pdb.set_trace()
