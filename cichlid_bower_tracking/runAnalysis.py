6d0a8f219379690341beede7aa111fba54ad0678
Patrick McGrath <pmcgrath7@ipsec-10-2-224-252.vpn.gatech.edu>
2026-01-16 17:11:08 -0500

    Small updates

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -310,9 +310,8 @@ elif args.AnalysisType == 'Summary':
 		sp_obj.downloadData()
 		sp_obj.validateInputData()
 		sp_obj.createSummaryFigures()
-		sp_obj.uploadData(delete=False)
+		sp_obj.uploadData(delete=True)
 		
-		pdb.set_trace()
 		fm_obj = FM(analysisID, projectID)
 		s_dt = fm_obj.s_dt
 		s_dt.loc[projectID,'Summary'] = True
