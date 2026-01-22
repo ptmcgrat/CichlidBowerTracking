b37bc31658e2571a2e016000e9b69fda906fc88f
Patrick McGrath <pmcgrath7@ipsec-10-2-224-252.vpn.gatech.edu>
2026-01-22 15:09:50 -0500

    Small updates

--- cichlid_bower_tracking/data_preparers/threeD_model_preparer.py
+++ cichlid_bower_tracking/data_preparers/threeD_model_preparer.py
@@ -61,7 +61,7 @@ class ThreeDModelPreparer():
 		#command = "source " + os.getenv('HOME') + "/anaconda3/etc/profile.d/conda.sh; conda activate CichlidActionClassification; " + ' '.join(command)
 		os.chdir('CAC_MD')
 		subprocess.run(['git', 'pull'])
-		#subprocess.run(command)
+		subprocess.run(command)
 		os.chdir('..')
 		
 		with open(os.path.join(self.fileManager.local3DModelDir,'val.log')) as f:
