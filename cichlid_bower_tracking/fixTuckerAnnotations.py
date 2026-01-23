b9858b8f2581e4ad4c5eb0e81c607c800cae557e
Patrick McGrath <pmcgrath7@ipsec-10-2-224-252.vpn.gatech.edu>
2026-01-23 14:45:37 -0500

    Small updates

--- cichlid_bower_tracking/fixTuckerAnnotations.py
+++ cichlid_bower_tracking/fixTuckerAnnotations.py
@@ -1,4 +1,4 @@
-import subprocess,pdb,shutil
+import subprocess,pdb,shutil,os
 from helper_modules.file_manager import FileManager as FM
 
 fm_obj = FM()
@@ -14,13 +14,13 @@ fm_obj.createDirectory(hybrid_outdata + 'labels/val/')
 fm_obj.createDirectory(hybrid_outdata)
 
 with open(hybrid_outdata + 'data.yaml', 'w') as f:
-	print('path: ' + hybrid_outdata)
-	print('train: images/train')
-	print('val: images/val')
-	print('')
-	print('names:')
-	print('  0: female')
-	print('  1: male')
+	print('path: ' + hybrid_outdata, file = f)
+	print('train: images/train', file = f)
+	print('val: images/val', file = f)
+	print('', file = f)
+	print('names:', file = f)
+	print('  0: female', file = f)
+	print('  1: male', file = f)
 
 train_images = os.listdir(hybrid_indata + 'images/train/') 
 for ti in train_images:
@@ -34,9 +34,9 @@ for ti in train_labels:
 	with open(hybrid_indata + 'labels/train/' + ti) as infile, open(hybrid_outdata + 'labels/train' + ti) as outfile:
 		for line in infile:
 			if line[0] == '0':
-				print('1' + line[1:], file = outfile)
+				print('1' + line[1:], file = outfile, file = f)
 			elif line[0] == '1':
-				print('0' + line[1:], file = outfile)
+				print('0' + line[1:], file = outfile, file = f)
 			else:
 				raise Exception
 
@@ -45,9 +45,9 @@ for ti in val_labels:
 	with open(hybrid_indata + 'labels/val/' + ti) as infile, open(hybrid_outdata + 'labels/val' + ti) as outfile:
 		for line in infile:
 			if line[0] == '0':
-				print('1' + line[1:], file = outfile)
+				print('1' + line[1:], file = outfile, file = f)
 			elif line[0] == '1':
-				print('0' + line[1:], file = outfile)
+				print('0' + line[1:], file = outfile, file = f)
 			else:
 				raise Exception
 
