d6e38a00f07445e8824cdc7de05f839d7deb4cd0
Patrick McGrath <pmcgrath7@ipsec-10-2-224-252.vpn.gatech.edu>
2026-01-23 14:46:06 -0500

    Small updates

--- cichlid_bower_tracking/fixTuckerAnnotations.py
+++ cichlid_bower_tracking/fixTuckerAnnotations.py
@@ -34,9 +34,9 @@ for ti in train_labels:
 	with open(hybrid_indata + 'labels/train/' + ti) as infile, open(hybrid_outdata + 'labels/train' + ti) as outfile:
 		for line in infile:
 			if line[0] == '0':
-				print('1' + line[1:], file = outfile, file = f)
+				print('1' + line[1:], file = outfile)
 			elif line[0] == '1':
-				print('0' + line[1:], file = outfile, file = f)
+				print('0' + line[1:], file = outfile)
 			else:
 				raise Exception
 
@@ -45,9 +45,9 @@ for ti in val_labels:
 	with open(hybrid_indata + 'labels/val/' + ti) as infile, open(hybrid_outdata + 'labels/val' + ti) as outfile:
 		for line in infile:
 			if line[0] == '0':
-				print('1' + line[1:], file = outfile, file = f)
+				print('1' + line[1:], file = outfile)
 			elif line[0] == '1':
-				print('0' + line[1:], file = outfile, file = f)
+				print('0' + line[1:], file = outfile)
 			else:
 				raise Exception
 
