4107916212b790aec206d91bdcb040efa8267818
Patrick McGrath <pmcgrath7@ipsec-10-2-97-199.vpn.gatech.edu>
2026-01-25 09:48:11 -0500

    Small updates

--- cichlid_bower_tracking/fixTuckerAnnotations.py
+++ cichlid_bower_tracking/fixTuckerAnnotations.py
@@ -16,7 +16,7 @@ for projectID, row in fm_obj.s_dt.iterrows():
 	results = model.track(videoObj.localVideoFile, stream = True)  # Tracking with default tracker
 	for result in results:
 		track_ids = result.boxes.id.cpu().tolist()
-		print(f"Frame has track IDs: {track_ids}")
+		#print(f"Frame has track IDs: {track_ids}")
 """
 hybrid_indata = fm_obj.localYOLOAnnotationDir + 'OriginalTuckerData/CVxMC-tucker-2025-11-13/'
 hybrid_outdata = fm_obj.localYOLOAnnotationDir + 'HybridMulti/'
