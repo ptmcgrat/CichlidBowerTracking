e441f70be0cfa74b696921ca9ad7dbeae4229815
Patrick McGrath <pmcgrath7@ipsec-10-2-97-199.vpn.gatech.edu>
2026-01-28 15:25:44 -0500

    Small updates

--- cichlid_bower_tracking/fixMissingVideos.py
+++ cichlid_bower_tracking/fixMissingVideos.py
@@ -21,17 +21,20 @@ dt['VideoInfo'] = dt.ClipName.str.split('__').str[1]
 for lid,row in dt.iterrows():
 	clip_location = fm_obj.localLabeledClipsDir + row.ClipName
 	dt.loc[lid,'ClipExists'] = os.path.exists(clip_location)
+
 print(len(dt[dt.ClipExists == False]))
 missing_projects = dt[dt.ClipExists == False].groupby('ProjectID').count().index.tolist()
 for projectID in missing_projects:
 	videoIndices = [int(x) for x in s_dt.loc[projectID,'videoIDsToAnnotate'].split(': ')[1].split(',')]
 	fm_obj.setProjectID(projectID)
+	local_project_dir = m_obj.localLabeledClipsDir + projectID + '/'
+	fm_obj.downloadData(local_project_dir, tarred = True)
 	for videoIndex in videoIndices:
 		videoObj = fm_obj.returnVideoObject(videoIndex)
 		fm_obj.downloadData(videoObj.localManualLabelClipsDir, tarred = True)
 
 	for lid,row in dt[(dt.ClipExists == False) & (dt.ProjectID == projectID)].iterrows():
-		clip_location1 = fm_obj.localLabeledClipsDir + row.ClipName
+		clip_location1 = local_project_dir + row.ClipName
 		clip_location1a = clip_location1.replace('.mp4','_ManualLabel.mp4')
 		clip_location2 = fm_obj.localManualLabelClipsDir + row.VideoInfo + '/' + row.ClipName.replace(row.ProjectID + '__','')
 		clip_location2a = clip_location2.replace('.mp4','_ManualLabel.mp4')
@@ -39,5 +42,4 @@ for projectID in missing_projects:
 			subprocess.run(['cp', clip_location2,clip_location1])
 			subprocess.run(['cp', clip_location2a,clip_location1a])
 
-for projectID in missing_projects:
-	fm_obj.uploadData(fm_obj.localLabeledClipsDir + projectID, tarred = True)
+	fm_obj.uploadData(local_project_dir, tarred = True)
