5df7cbd0d93b498c4ba111f19e869da73f93262f
Patrick McGrath <pmcgrath7@ipsec-10-2-224-6.vpn.gatech.edu>
2026-01-29 05:45:02 -0500

    Small updates

--- cichlid_bower_tracking/fixMissingVideos.py
+++ cichlid_bower_tracking/fixMissingVideos.py
@@ -12,7 +12,7 @@ fm_obj.downloadData(fm_obj.localLabeledClipsDir, tarred_subdirs = True)
 fm_obj.downloadData(fm_obj.localLabeledClipsFile)
 
 dt = pd.read_csv(fm_obj.localLabeledClipsFile, index_col = 0)
-dt = dt[dt.AnalysisID == 'YH_MC_Parentals']
+dt = dt[dt.AnalysisID == 'HybridMulti']
 dt['ClipExists'] = True
 dt['ProjectID'] = dt.ClipName.str.split('__').str[0]
 dt['ClipName'] = dt.ClipName + '.mp4'
@@ -24,6 +24,7 @@ for lid,row in dt.iterrows():
 
 print(len(dt[dt.ClipExists == False]))
 missing_projects = dt[dt.ClipExists == False].groupby('ProjectID').count().index.tolist()
+pdb.set_trace()
 for projectID in missing_projects:
 	videoIndices = [int(x) for x in s_dt.loc[projectID,'videoIDsToAnnotate'].split(': ')[1].split(',')]
 	fm_obj.setProjectID(projectID)
