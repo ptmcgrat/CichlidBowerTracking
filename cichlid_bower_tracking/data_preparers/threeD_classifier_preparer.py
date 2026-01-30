00843b19ad7be641b49dcac3367bf5328452fa17
Patrick McGrath <pmcgrath7@ipsec-10-2-224-6.vpn.gatech.edu>
2026-01-30 07:27:29 -0500

    Small updates

--- cichlid_bower_tracking/data_preparers/threeD_classifier_preparer.py
+++ cichlid_bower_tracking/data_preparers/threeD_classifier_preparer.py
@@ -50,7 +50,7 @@ class ThreeDClassifierPreparer:
 			data = {}
 			data['ClipName'] = [x for x in os.listdir(videoObj.localAllClipsDir) if '.mp4' in x]
 			data['ProjectID'] = self.fileManager.projectID
-			data['ManuelLabel'] = 'x' # Doesn't matter what you put in here
+			data['ManualLabel'] = 'x' # Doesn't matter what you put in here
 			data['AnalysisID'] = self.fileManager.analysisID
 			dt = pd.DataFrame(data)
 			dt.to_csv(self.fileManager.localVideoProjectsFile)
