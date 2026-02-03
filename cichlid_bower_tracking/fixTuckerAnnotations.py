70a74f2aa0e505c424c3a33f22eb7adff64f9ac5
Patrick McGrath <pmcgrath7@ipsec-10-2-224-6.vpn.gatech.edu>
2026-02-03 10:15:11 -0500

    Small updates

--- cichlid_bower_tracking/fixTuckerAnnotations.py
+++ cichlid_bower_tracking/fixTuckerAnnotations.py
@@ -1,4 +1,4 @@
-import subprocess,pdb,shutil,os
+import subprocess,pdb,shutil,os,csv
 from helper_modules.file_manager import FileManager as FM
 from ultralytics import YOLO
 
@@ -17,11 +17,19 @@ for projectID, row in fm_obj.s_dt.iterrows():
 	fm_obj.setProjectID(projectID)
 	videoObj = fm_obj.returnVideoObject(videoIndices[0])
 	fm_obj.downloadData(fm_obj.localLabeledDLCClipsDir + videoObj.localDLCVideoFile)
+	with open('tracking_results.csv', 'w', newline='') as f:
+		writer = csv.writer(f)
+		results = model.track(fm_obj.localLabeledDLCClipsDir + videoObj.localDLCVideoFile, stream = True, save=True, show=False, persist = True)  # Tracking with default tracker
+		for result in results:
+			if result.boxes.id is not None:
+				boxes = results.boxes.xywh.cpu().numpy()  # Convert to numpy for easy manipulation
+            	track_ids = results.boxes.id.cpu().numpy().astype(int)
+	            for box, track_id in zip(boxes, track_ids):
+                	x_center, y_center, width, height = box
+                	# Write frame, ID, and coordinates to the CSV file
+                	writer.writerow([frame_idx, track_id, x_center, y_center, width, height])
 
-	results = model.track(fm_obj.localLabeledDLCClipsDir + videoObj.localDLCVideoFile, stream = True, save=True, show=False)  # Tracking with default tracker
-	for result in results:
-		next(results)
-	#	if result.boxes.id is not None:
+
 	#		track_ids = result.boxes.id.cpu().tolist()
 	#		#print(f"Frame has track IDs: {track_ids}")
 	pdb.set_trace()
