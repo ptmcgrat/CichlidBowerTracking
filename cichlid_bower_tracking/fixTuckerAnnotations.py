be05e257267eb9b46ae1d128ddc144cdcbb01395
Patrick McGrath <pmcgrath7@ipsec-10-2-224-6.vpn.gatech.edu>
2026-02-03 09:16:09 -0500

    Small updates

--- cichlid_bower_tracking/fixTuckerAnnotations.py
+++ cichlid_bower_tracking/fixTuckerAnnotations.py
@@ -16,9 +16,9 @@ for projectID, row in fm_obj.s_dt.iterrows():
 	videoIndices = [int(x) for x in row.videoIDsToAnnotate.split(': ')[1].split(',')]
 	fm_obj.setProjectID(projectID)
 	videoObj = fm_obj.returnVideoObject(videoIndices[0])
-	fm_obj.downloadData(videoObj.localVideoFile)
+	fm_obj.downloadData(self.fileManager.localLabeledDLCClipsDir + videoObj.localDLCVideoFile)
 
-	results = model.track(videoObj.localVideoFile, stream = False, save=True, show=False)  # Tracking with default tracker
+	results = model.track(self.fileManager.localLabeledDLCClipsDir + videoObj.localDLCVideoFile, stream = False, save=True, show=False)  # Tracking with default tracker
 	#for result in results:
 	#	if result.boxes.id is not None:
 	#		track_ids = result.boxes.id.cpu().tolist()
