089d2fb685dd9ffbeda6abe38b92c47ec2b7b524
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-04 19:43:13 -0500

    Small updates

--- cichlid_bower_tracking/data_preparers/edit_videos_preparer.py
+++ cichlid_bower_tracking/data_preparers/edit_videos_preparer.py
@@ -57,33 +57,36 @@ class EditVideosPreparer():
 	def editVideos(self):
 		
 		self.cl_obj = CA(self.fileManager)
-		t_dt = pd.read_csv(self.fileManager.localAllFishTracksFile)
+		
 
 		for videoIndex in self.videoIndices:
 			videoObj = self.fileManager.returnVideoObject(videoIndex)
 			cap = cv2.VideoCapture(videoObj.localVideoFile)
 			out_file = self.fileManager.localEditVideosDir + videoObj.baseName + '.mp4'
 			outAll = cv2.VideoWriter(out_file, cv2.VideoWriter_fourcc(*"mp4v"), videoObj.framerate, (videoObj.width, videoObj.height))
-			for i in range(int(videoObj.framerate*60*15)):
+			t_dt = pd.read_csv(self.fileManager.localAllFishTracksFile)
+			t_dt = t_dt[(t_dt.VideoID == videoObj.baseName)]
+			for i in range(int(videoObj.framerate*60*135)):
 				current_time = videoObj.startTime + datetime.timedelta(seconds = i/videoObj.framerate)
-				out_dt = self.cl_obj.addClusterLabels(current_time, videoObj)
 				ret, frame = cap.read()
-				pdb.set_trace()
-				sub_dt = t_dt[(t_dt.VideoID == videoObj.baseName) & (t_dt.FrameNum == i)]
+				if i < videoObj.framerate*60*120:
+					continue
+				out_dt = self.cl_obj.addClusterLabels(current_time, videoObj)
+				
+				sub_dt = t_dt[(t_dt.FrameNum == i)]
 				if ret:
 					for time, row in out_dt.iterrows():
 						cv2.rectangle(frame, (row.x1, row.y1), (row.x2, row.y2), color=row.color, thickness=2)
 						cv2.putText(frame, row.label, (row.x1, row.y1), cv2.FONT_HERSHEY_SIMPLEX, 1, row.color, 2)
-					if len(sub_dt) != 0:
-						for t_id, row in sub_dt.iterrows():
-							if row.Sex == 'female':
-								cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(255,0,0), thickness=2)
-							if row.Sex == 'male':
-								cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(0,0,255), thickness=2)
-							if row.TrackID in out_dt.track_id:
-								for time,row in out_dt[out_dt.track_id == row.TrackID]:
-									cv2.line(img, (row.X_center, row.Y_center), ((row.x1 + row.x2)/2, (row.y1 + row.y2)/2), (122, 122, 122), 2)
-		
+					for t_id, row in sub_dt.iterrows():
+						if row.Sex == 'female':
+							cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(255,0,0), thickness=2)
+						if row.Sex == 'male':
+							cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(0,0,255), thickness=2)
+						if row.TrackID in out_dt.track_id:
+							for time,row in out_dt[out_dt.track_id == row.TrackID]:
+								cv2.line(img, (row.X_center, row.Y_center), ((row.x1 + row.x2)/2, (row.y1 + row.y2)/2), (122, 122, 122), 2)
+	
 					outAll.write(frame)
 				else:
 					break
