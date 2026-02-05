87bf576956b6fe956629bcad3e032f82e812f0f3
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-05 08:21:49 -0500

    Small updates

--- cichlid_bower_tracking/data_preparers/edit_videos_preparer.py
+++ cichlid_bower_tracking/data_preparers/edit_videos_preparer.py
@@ -85,9 +85,8 @@ class EditVideosPreparer():
 						if row.Sex == 'male':
 							cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(0,0,255), thickness=2)
 						if row.TrackID in out_dt.track_id.to_list():
-							for time,row in out_dt[out_dt.track_id == row.TrackID].iterrows():
-								pdb.set_trace()
-								cv2.line(frame, (row.X_center, row.Y_center), ((row.x1 + row.x2)/2, (row.y1 + row.y2)/2), (122, 122, 122), 2)
+							for time2,row2 in out_dt[out_dt.track_id == row.TrackID].iterrows():
+								cv2.line(frame, (row.X_center, row.Y_center), ((row2.x1 + row2.x2)/2, (row2.y1 + row2.y2)/2), (122, 122, 122), 2)
 	
 					outAll.write(frame)
 				else:
