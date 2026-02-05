9c5a3dd456aa36c74338d82d2e362d18d38f2a80
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-05 11:20:56 -0500

    Small updates

--- cichlid_bower_tracking/data_preparers/associate_tracks_preparer.py
+++ cichlid_bower_tracking/data_preparers/associate_tracks_preparer.py
@@ -74,8 +74,10 @@ class AssociateTracksPreparer:
 				min_distance = (((row.X - sub_dt['Y_center']) ** 2 + (row.Y - sub_dt['X_center']) ** 2) ** 0.5).min()
 				if min_distance < 60:
 					track_index = (((row.X - sub_dt['Y_center']) ** 2 + (row.Y - sub_dt['X_center']) ** 2) ** 0.5).idxmin()
-				c_dt.loc[lid,'TrackID'] = int(dt.loc[track_index]['TrackID'])
-			
+				try:
+					c_dt.loc[lid,'TrackID'] = int(dt.loc[track_index]['TrackID'])
+				except:
+					pdb.set_trace()		
 			try:
 				# c_dt = c_dt.append(new_dt)
 				track_dt = pd.concat([track_dt, dt], ignore_index=True)
