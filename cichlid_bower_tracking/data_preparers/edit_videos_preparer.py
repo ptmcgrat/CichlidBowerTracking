71cd2ad71f770f6f514e4805076f7155abf2860f
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-05 08:54:50 -0500

    Small updates

--- cichlid_bower_tracking/data_preparers/edit_videos_preparer.py
+++ cichlid_bower_tracking/data_preparers/edit_videos_preparer.py
@@ -81,13 +81,12 @@ class EditVideosPreparer():
 						cv2.putText(frame, row.label, (row.x1, row.y1), cv2.FONT_HERSHEY_SIMPLEX, 1, row.color, 2)
 					for t_id, row in sub_dt.iterrows():
 						if row.Sex == 'female':
-							cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(255,0,0), thickness=2)
-						if row.Sex == 'male':
 							cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(0,0,255), thickness=2)
+						if row.Sex == 'male':
+							cv2.rectangle(frame, (int(row.X_center - row.Width/2), int(row.Y_center - row.Height/2)), (int(row.X_center + row.Width/2), int(row.Y_center + row.Height/2)), color=(255,0,0), thickness=2)
 						if row.TrackID in out_dt.track_id.to_list():
 							for time2,row2 in out_dt[out_dt.track_id == row.TrackID].iterrows():
-								pdb.set_trace()
-								cv2.line(frame, (row.X_center, row.Y_center), (int((row2.x1 + row2.x2)/2), int((row2.y1 + row2.y2)/2)), (122, 122, 122), 2)
+								cv2.line(frame, (int(row.X_center), (row.Y_center)), (int((row2.x1 + row2.x2)/2), int((row2.y1 + row2.y2)/2)), (122, 122, 122), 2)
 	
 					outAll.write(frame) 
 				else:
