e44dd6ea13bf314428e7c3739f87226b413dab86
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-06 13:13:43 -0500

    Small updates

--- cichlid_bower_tracking/data_preparers/summary_preparer.py
+++ cichlid_bower_tracking/data_preparers/summary_preparer.py
@@ -76,7 +76,7 @@ class SummaryPreparer:
 		for i,trial in enumerate(self.lp.trials):
 			localTrialFigureFile = self.fileManager.localSummaryDir + trial.figureFile
 			num_days = len(trial.days)
-			figTrial, axes = plt.subplots(nrows = 24, ncols = num_days, figsize=(num_days, 24), squeeze=False)
+			figTrial, axes = plt.subplots(nrows = 25, ncols = num_days, figsize=(num_days, 25), squeeze=False)
 			figTrial.suptitle(self.lp.projectID + ' Trial ' + str(i+1) + ' Summary File')
 			start_frame = trial.days[0][0]
 
