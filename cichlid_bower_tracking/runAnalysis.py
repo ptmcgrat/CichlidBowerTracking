b71536cff76778c11afae464cd22d2fd4b22ad75
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-07 13:43:03 -0500

    Small updates

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -330,7 +330,7 @@ elif args.AnalysisType == 'TrackFish':
 		print('Tracking: ' + str(datetime.datetime.now()))
 		tfp_obj.runYOLOAnalysis()
 		print('Uploading: ' + str(datetime.datetime.now()))		
-		tfp_obj.uploadProjectData(delete = True)
+		tfp_obj.uploadProjectData(delete = False)
 		print('Done: ' + str(datetime.datetime.now()))
 
 		fm_obj = FM(analysisID, projectID)
