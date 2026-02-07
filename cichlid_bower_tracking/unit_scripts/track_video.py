a6ceb2507fcdc8c4f24994de35d33b6a10d3ce48
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-07 13:41:03 -0500

    Small updates

--- cichlid_bower_tracking/unit_scripts/track_video.py
+++ cichlid_bower_tracking/unit_scripts/track_video.py
@@ -5,31 +5,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument('InputVideo', type = str, help = 'Name of logfile')
 parser.add_argument('OutputCSV', type = str, help = 'Name of logfile')
 parser.add_argument('YOLOModel', type = str, help = 'Name of logfile')
-parser.add_argument('AnalysisID', type = str, help = 'Name of logfile')
-parser.add_argument('ProjectID', type = str, help = 'Name of logfile')
-
 
 args = parser.parse_args()
 
 
-model = YOLO(self.fileManager.localYOLOModelFile)
+model = YOLO(args.YOLOModel)
 
-		with open(self.videoObj.localFishDetectionsFile, 'w', newline='') as f:
-		
-			writer = csv.writer(f)
-			writer.writerow(['FrameNum', 'TrackID', 'X_center', 'Y_center', 'Width', 'Height', 'SexID', 'Sex'])
+with open(args.OutputCSV, 'w', newline='') as f:
 
-			results = model.track(self.videoObj.localVideoFile, stream = True, save=False, show=False, persist = True, verbose = False)  # Tracking with default tracker
-			print('TrackingStart: ' + str(datetime.datetime.now()))
-		
-			for frame_idx, result in enumerate(results):
-				if result.boxes.id is not None:
-					boxes = result.boxes.xywh.cpu().numpy()  # Convert to numpy for easy manipulation
-					track_ids = result.boxes.id.cpu().numpy().astype(int)
-					classes = result.boxes.cls.cpu().numpy()
-					for box, track_id, class_id in zip(boxes, track_ids, classes):
-						x_center, y_center, width, height = box
-						# Write frame, ID, and coordinates to the CSV file
-						writer.writerow([frame_idx, track_id, x_center, y_center, width, height,class_id, result.names[class_id]])
+	writer = csv.writer(f)
+	writer.writerow(['FrameNum', 'TrackID', 'X_center', 'Y_center', 'Width', 'Height', 'SexID', 'Sex'])
 
-		print('TrackingEnd: ' + str(datetime.datetime.now()))
+	results = model.track(args.InputVideo, stream = True, save=False, show=False, persist = True, verbose = False)  # Tracking with default tracker
+	#print('TrackingStart: ' + str(datetime.datetime.now()))
+
+	for frame_idx, result in enumerate(results):
+		if result.boxes.id is not None:
+			boxes = result.boxes.xywh.cpu().numpy()  # Convert to numpy for easy manipulation
+			track_ids = result.boxes.id.cpu().numpy().astype(int)
+			classes = result.boxes.cls.cpu().numpy()
+			for box, track_id, class_id in zip(boxes, track_ids, classes):
+				x_center, y_center, width, height = box
+				# Write frame, ID, and coordinates to the CSV file
+				writer.writerow([frame_idx, track_id, x_center, y_center, width, height,class_id, result.names[class_id]])
+
