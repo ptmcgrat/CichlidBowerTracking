c7d2d28e4f2883686812cb705cd11effa183c8bd
Patrick McGrath <pmcgrath7@ipsec-10-2-96-24.vpn.gatech.edu>
2026-02-07 07:54:24 -0500

    Small updates

--- cichlid_bower_tracking/helper_modules/file_manager.py
+++ cichlid_bower_tracking/helper_modules/file_manager.py
@@ -179,7 +179,7 @@ class FileManager():
             elif analysisType == 'TrackFish':
                 projectIDs = s_dt[(s_dt.RunAnalysis == True)].index.to_list()
             elif analysisType == 'AssociateTracksWithClusters':
-                projectIDs = s_dt[(s_dt.RunAnalysis == True)].index.to_list()
+                projectIDs = s_dt[(s_dt.RunAnalysis == True) & (s_dt.TrackFish != 'VideoIndices: ') & (s_dt[analysisType] == False)].index.to_list()
             elif analysisType == 'Summary':
                 projectIDs = s_dt[(s_dt.Depth == True) & (s_dt.ClassifyClusters == True) & (s_dt.RunAnalysis == True) & (s_dt[analysisType] == False)].index.to_list()
             elif analysisType == 'EditVideos':
