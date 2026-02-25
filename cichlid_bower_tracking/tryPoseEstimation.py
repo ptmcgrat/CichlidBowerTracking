84fad431991ee045513b369c889625a7128bc183
Patrick McGrath <pmcgrath7@ipsec-10-2-96-144.vpn.gatech.edu>
2026-02-25 18:40:34 -0500

    Small updates

--- cichlid_bower_tracking/tryPoseEstimation.py
+++ cichlid_bower_tracking/tryPoseEstimation.py
@@ -5,7 +5,8 @@ from ultralytics import YOLO
 directories = ['CVxMC-tucker-2025-11-13/','MC-tucker-2025-11-03/','MCxYH-tucker-2025-11-03/','PD-tucker-2025-11-10/','YH-tucker-2025-11-03/']
 directories = ['Benthics/']
 fm_obj = FM(analysisID = 'YH_MC_Parentals')
-
+fm_obj.uploadData(fm_obj.localMLPoseDir + directory)
+pdb.set_trace()
 for directory in directories:
 	trial_data = fm_obj.localPoseDir + directory
 	fm_obj.downloadData(trial_data)
@@ -14,4 +15,4 @@ for directory in directories:
 	model = YOLO("yolo26l-pose.pt")  # build a new model from YAML
 	results = model.train(data=yaml_file, epochs = 200, imgsz=640, project = fm_obj.localMLPoseDir, name=directory.replace('/','_l/'), batch=-1)
 	fm_obj.uploadData(fm_obj.localMLPoseDir + directory.replace('/','_l/'))
-	fm_obj.uploadData(fm_obj.localMLPoseDir + directory.replace('/','_l/'))
+	fm_obj.uploadData(fm_obj.localMLPoseDir + directory)
