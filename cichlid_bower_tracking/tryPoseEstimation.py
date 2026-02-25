3d64e5a7554c695f1bc9d491abd68dfda0e60dc2
Patrick McGrath <pmcgrath7@ipsec-10-2-96-144.vpn.gatech.edu>
2026-02-25 10:35:14 -0500

    Small updates

---
+++ cichlid_bower_tracking/tryPoseEstimation.py
@@ -0,0 +1,12 @@
+import subprocess,pdb,shutil,os,csv, datetime
+from helper_modules.file_manager import FileManager as FM
+from ultralytics import YOLO
+
+fm_obj = FM(analysisID = 'YH_MC_Parentals')
+trial_data = self.localPoseDir + 'MCxYH-tucker-2025-11-03/'
+fm_obj.downloadData(trial_data)
+
+yaml_file = trial_data + 'data.yaml'
+model = YOLO("yolo26n-pose.yaml")  # build a new model from YAML
+results = model.train(data=yaml_file, epochs = 100, imgsz=640)
+
