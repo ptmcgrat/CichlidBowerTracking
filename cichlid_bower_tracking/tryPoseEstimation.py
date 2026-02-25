38f477556bb44f613370f6f0383530568f4b2c0f
Patrick McGrath <pmcgrath7@ipsec-10-2-96-144.vpn.gatech.edu>
2026-02-25 16:01:37 -0500

    Small updates

--- cichlid_bower_tracking/tryPoseEstimation.py
+++ cichlid_bower_tracking/tryPoseEstimation.py
@@ -3,7 +3,7 @@ from helper_modules.file_manager import FileManager as FM
 from ultralytics import YOLO
 
 directories = ['CVxMC-tucker-2025-11-13/','MC-tucker-2025-11-03/','MCxYH-tucker-2025-11-03/','PD-tucker-2025-11-10/','YH-tucker-2025-11-03/']
-directories = ['Benthics']
+directories = ['Benthics/']
 fm_obj = FM(analysisID = 'YH_MC_Parentals')
 
 for directory in directories:
