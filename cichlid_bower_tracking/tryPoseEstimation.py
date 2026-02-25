670f81cb342bc52e7d968c2cd075149282d6eaf0
Patrick McGrath <pmcgrath7@ipsec-10-2-96-144.vpn.gatech.edu>
2026-02-25 14:01:37 -0500

    Small updates

--- cichlid_bower_tracking/tryPoseEstimation.py
+++ cichlid_bower_tracking/tryPoseEstimation.py
@@ -2,11 +2,13 @@ import subprocess,pdb,shutil,os,csv, datetime
 from helper_modules.file_manager import FileManager as FM
 from ultralytics import YOLO
 
+directories = ['CVxMC-tucker-2025-11-13','MC-tucker-2025-11-03','MCxYH-tucker-2025-11-03/','PD-tucker-2025-11-10','YH-tucker-2025-11-03']
 fm_obj = FM(analysisID = 'YH_MC_Parentals')
-trial_data = fm_obj.localPoseDir + 'MCxYH-tucker-2025-11-03/'
-fm_obj.downloadData(trial_data)
 
-yaml_file = trial_data + 'data.yaml'
-model = YOLO("yolo26n-pose.pt")  # build a new model from YAML
-results = model.train(data=yaml_file, epochs = 200, imgsz=640)
-pdb.set_trace()
+for directory in directories:
+	trial_data = fm_obj.localPoseDir + directory
+	fm_obj.downloadData(trial_data)
+
+	yaml_file = trial_data + 'data.yaml'
+	model = YOLO("yolo26n-pose.pt")  # build a new model from YAML
+	results = model.train(data=yaml_file, epochs = 200, imgsz=640,name=directory)
