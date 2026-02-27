57192870b45a8e8ecf8e40c866a45b0e75e0eece
Patrick McGrath <pmcgrath7@ipsec-10-2-96-144.vpn.gatech.edu>
2026-02-27 18:51:59 -0500

    Small updates

--- cichlid_bower_tracking/tryPoseEstimation.py
+++ cichlid_bower_tracking/tryPoseEstimation.py
@@ -13,9 +13,9 @@ fm_obj = FM(analysisID = 'YH_MC_Parentals')
 test_file = fm_obj.localLabeledDLCClipsDir + 'MCYHF1_549_t011_tr1__0009_vid__DLC.mp4'
 fm_obj.downloadData(test_file)
 fm_obj.downloadData(fm_obj.localYOLOModelDir)
-fm_obj.downloadData(fm_obj.localMLPoseDir + 'Benthics_l/weights/best.pt')
+fm_obj.downloadData(fm_obj.localMLPoseDir + 'Benthics/weights/best.pt')
 
-model_pose = YOLO(fm_obj.localMLPoseDir + 'Benthics_l/weights/best.pt')
+model_pose = YOLO(fm_obj.localMLPoseDir + 'Benthics/weights/best.pt')
 model_track = YOLO(fm_obj.localYOLOModelFile)
 
 results = model_pose.track(test_file, show=True, device=device)
@@ -25,7 +25,7 @@ results = model_pose.track(test_file, show=True, device=device)
 fm_obj = FM(analysisID = 'YH_MC_Parentals')
 
 fm_obj.downloadData(fm_obj.localPoseDir + 'Benthics/')
-model = YOLO("yolo26n-pose.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo26x-pose.pt")  # load a pretrained model (recommended for training)
 
-results = model.train(data=fm_obj.localPoseDir + 'Benthics/data.yaml', epochs=100, imgsz=640, project = fm_obj.localMLPoseDir, name = 'Benthics')
+results = model.train(data=fm_obj.localPoseDir + 'Benthics/data.yaml', epochs=100, imgsz=640, project = fm_obj.localMLPoseDir, name = 'Benthics', batch = -1, exist_ok=True)
 fm_obj.uploadData(fm_obj.localMLPoseDir + 'Benthics/')
