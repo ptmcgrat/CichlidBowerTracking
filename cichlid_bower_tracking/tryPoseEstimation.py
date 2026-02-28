626c5b0c6640103400fcd5ff596589492792407d
Patrick McGrath <pmcgrath7@ipsec-10-2-96-144.vpn.gatech.edu>
2026-02-28 15:08:17 -0500

    Small updates

--- cichlid_bower_tracking/tryPoseEstimation.py
+++ cichlid_bower_tracking/tryPoseEstimation.py
@@ -66,77 +66,146 @@ def modifyTuckerObjectDetections():
 
 def modifyTuckerLabels():
 	fm_obj = FM(analysisID = 'YH_MC_Parentals')
-	indatas = ['CVxMC-tucker-2025-11-13/', 'MC-tucker-2025-11-03/', 'MCxYH-tucker-2025-11-03/', 'YH-tucker-2025-11-03/']
+	main_dir_pose = fm_obj.localAnnotationDir + 'TuckerAnnotations/bbox_and_pose/'
+	main_dir_detect = fm_obj.localAnnotationDir + 'TuckerAnnotations/bbbox_only/OriginalTuckerData/'
+	fm_obj.downloadData(main_dir_pose)
+	fm_obj.downloadData(main_dir_detect)
+
+	projects = ['CVxMC-tucker-2025-11-13/', 'MC-tucker-2025-11-03/', 'MCxYH-tucker-2025-11-03/', 'YH-tucker-2025-11-03/']
 	#for dtype,outdir in zip(['bbbox_only/OriginalTuckerData/','bbox_and_pose/'],[fm_obj.localObjectDetectionDir + 'GenericSex/', fm_obj.localPoseDir + 'GenericPose/']):
 
-	outdir = fm_obj.localPoseDir + 'GenericPose/'
-	main_dir = fm_obj.localAnnotationDir + 'TuckerAnnotations/bbox_and_pose/'
-	fm_obj.downloadData(main_dir)
-	fm_obj.createDirectory(outdir)
-	for d in ['images/train','images/val','labels/train','labels/val']:
-		fm_obj.createDirectory(outdir + d)
+	outdir_pose1 = fm_obj.localPoseDir + 'GenericPose1/'
+	outdir_pose2 = fm_obj.localPoseDir + 'GenericPose2/'
+	outdir_detect1 = fm_obj.localPoseDir + 'GenericSex1/'
+	outdir_detect2 = fm_obj.localPoseDir + 'GenericSex2/'
+	
+	for d in [outdir_pose1,outdir_pose2,outdir_detect1,outdir_detect2]:
+		fm_obj.createDirectory(d)
 
-	with open(outdir + 'data.yaml', 'w') as f:
-		print('path: ' + outdir, file = f)
-		print('train: images/train', file = f)
-		print('val: images/val', file = f)
-		print('', file = f)
-		print('kpt_shape: [10, 3]', file = f)
-		print('flip_idx: [0, 2, 1, 3, 4, 5, 6, 7, 8, 9]', file = f)
-		print('kpt_names:', file = f)
-		print('  - nose', file = f)
-		print('  - leftEye', file = f)
-		print('  - rightEye', file = f)
-		print('  - head', file = f)
-		print('  - spine1', file = f)
-		print('  - spine2', file = f)
-		print('  - spine3', file = f)
-		print('  - spine4', file = f)
-		print('  - peduncle', file = f)
-		print('  - tailTip', file = f)
-		print('', file = f)
-		print('names:', file = f)
-		print('  0: female', file = f)
-		print('  1: male', file = f)
+		for sd in ['images/train','images/val','labels/train','labels/val']:
+			fm_obj.createDirectory(d + sd)
 
-	for project in indatas:
-		subdir = main_dir + project
+	for d in [outdir_pose1,outdir_pose2]:
+		with open(d + 'data.yaml', 'w') as f:
+			print('path: ' + outdir, file = f)
+			print('train: images/train', file = f)
+			print('val: images/val', file = f)
+			print('', file = f)
+			print('kpt_shape: [10, 3]', file = f)
+			print('flip_idx: [0, 2, 1, 3, 4, 5, 6, 7, 8, 9]', file = f)
+			print('kpt_names:', file = f)
+			print('  - nose', file = f)
+			print('  - leftEye', file = f)
+			print('  - rightEye', file = f)
+			print('  - head', file = f)
+			print('  - spine1', file = f)
+			print('  - spine2', file = f)
+			print('  - spine3', file = f)
+			print('  - spine4', file = f)
+			print('  - peduncle', file = f)
+			print('  - tailTip', file = f)
+			print('', file = f)
+			print('names:', file = f)
+			print('  0: female', file = f)
+			print('  1: male', file = f)
 
-		train_images = os.listdir(subdir + 'images/train/') 
-		print(len(train_images))
-		for ti in train_images:
-			subprocess.run(['cp', subdir + 'images/train/' + ti, outdir + 'images/train/' + ti])
+	for d in [outdir_detect1,outdir_detect2]:
+		with open(d + 'data.yaml', 'w') as f:
+			print('path: ' + outdir, file = f)
+			print('train: images/train', file = f)
+			print('val: images/val', file = f)
+			print('', file = f)
+			print('names:', file = f)
+			print('  0: female', file = f)
+			print('  1: male', file = f)
+
+	for project in projects:
+		subdir = main_dir_pose + project
+		subdir_filt = main_dir_detect + project
 		
+		train_images = os.listdir(subdir + 'images/train/') 
+		train_images_filter = os.listdir(main_dir_detect + project + 'images/train/')
+
+		for ti in train_images:
+			subprocess.run(['cp', subdir + 'images/train/' + ti, outdir_pose1 + 'images/train/' + ti])
+			subprocess.run(['cp', subdir + 'images/train/' + ti, outdir_detect1 + 'images/train/' + ti])
+			with open(subdir + 'labels/train/' + ti) as infile, open(outdir_detect1 + 'labels/train/' + ti, 'w') as outfile_detect, open(outdir_pose1 + 'labels/train/' + ti, 'w') as outfile_pose:
+				for line in infile:
+					line = line.rstrip()
+					tokens = line.split(' ')
+
+					if line[0] == '0':
+						print('1' + line[1:], file = outfile_pose)
+						print('1' + ' '.join(tokens[1:5]), file = outfile_detect)
+					elif line[0] == '1':
+						print('0' + line.split(' '), file = outfile_pose)
+						print('0' + ' '.join(tokens[1:5]), file = outfile_detect)
+
+					else:
+						raise Exception
+
+			if ti in train_images_filter:
+				subprocess.run(['cp', subdir + 'images/train/' + ti, outdir_pose2 + 'images/train/' + ti])
+				subprocess.run(['cp', subdir + 'images/train/' + ti, outdir_detect2 + 'images/train/' + ti])
+
+				with open(subdir + 'labels/train/' + ti) as infile, open(outdir_detect2 + 'labels/train/' + ti, 'w') as outfile_detect, open(outdir_pose2 + 'labels/train/' + ti, 'w') as outfile_pose:
+					for line in infile:
+						line = line.rstrip()
+						tokens = line.split(' ')
+
+						if line[0] == '0':
+							print('1' + line[1:], file = outfile_pose)
+							print('1' + ' '.join(tokens[1:5]), file = outfile_detect)
+						elif line[0] == '1':
+							print('0' + line.split(' '), file = outfile_pose)
+							print('0' + ' '.join(tokens[1:5]), file = outfile_detect)
+
+						else:
+							raise Exception
+
 		val_images = os.listdir(subdir + 'images/val/') 
-		for ti in val_images:
-			subprocess.run(['cp', subdir + 'images/val/' + ti, outdir + 'images/val/' + ti])
-		print(project + ',Num_train: ' + str(len(train_images)) + ',Num_val: ' + str(len(val_images)))
+		val_images_filter = os.listdir(main_dir_detect + project + 'images/val/')
 
-		train_labels = os.listdir(subdir + 'labels/train/') 
-		for ti in train_labels:
-			with open(subdir + 'labels/train/' + ti) as infile, open(outdir + 'labels/train/' + ti, 'w') as outfile:
+		for vi in val_images:
+			subprocess.run(['cp', subdir + 'images/val/' + vi, outdir_pose1 + 'images/val/' + vi])
+			subprocess.run(['cp', subdir + 'images/val/' + vi, outdir_detect1 + 'images/val/' + vi])
+			with open(subdir + 'labels/val/' + vi) as infile, open(outdir_detect1 + 'labels/val/' + vi, 'w') as outfile_detect, open(outdir_pose1 + 'labels/val/' + vi, 'w') as outfile_pose:
 				for line in infile:
 					line = line.rstrip()
+					tokens = line.split(' ')
+
 					if line[0] == '0':
-						print('1' + line[1:], file = outfile)
+						print('1' + line[1:], file = outfile_pose)
+						print('1' + ' '.join(tokens[1:5]), file = outfile_detect)
 					elif line[0] == '1':
-						print('0' + line[1:], file = outfile)
+						print('0' + line.split(' '), file = outfile_pose)
+						print('0' + ' '.join(tokens[1:5]), file = outfile_detect)
+
 					else:
 						raise Exception
 
-		val_labels = os.listdir(subdir + 'labels/val/') 
-		for ti in val_labels:
-			with open(subdir + 'labels/val/' + ti) as infile, open(outdir + 'labels/val/' + ti, 'w') as outfile:
-				for line in infile:
-					line = line.rstrip()
-					if line[0] == '0':
-						print('1' + line[1:], file = outfile)
-					elif line[0] == '1':
-						print('0' + line[1:], file = outfile)
-					else:
-						raise Exception
+			if vi in val_images_filter:
+				subprocess.run(['cp', subdir + 'images/val/' + vi, outdir_pose2 + 'images/val/' + vi])
+				subprocess.run(['cp', subdir + 'images/val/' + vi, outdir_detect2 + 'images/val/' + vi])
 
-	fm_obj.uploadData(outdir)
+				with open(subdir + 'labels/val/' + vi) as infile, open(outdir_detect2 + 'labels/val/' + vi, 'w') as outfile_detect, open(outdir_pose2 + 'labels/val/' + vi, 'w') as outfile_pose:
+					for line in infile:
+						line = line.rstrip()
+						tokens = line.split(' ')
+
+						if line[0] == '0':
+							print('1' + line[1:], file = outfile_pose)
+							print('1' + ' '.join(tokens[1:5]), file = outfile_detect)
+						elif line[0] == '1':
+							print('0' + line.split(' '), file = outfile_pose)
+							print('0' + ' '.join(tokens[1:5]), file = outfile_detect)
+
+						else:
+							raise Exception
+		
+
+	for d in [outdir_pose1,outdir_pose2,outdir_detect1,outdir_detect2]:
+		fm_obj.uploadData(d)
 
 
 def train_model(mode):
