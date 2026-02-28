be233ce7b8b190f61b131d0fe57c0eb9a99b42a0
Patrick McGrath <pmcgrath7@ipsec-10-2-96-144.vpn.gatech.edu>
2026-02-28 08:15:50 -0500

    Small updates

--- cichlid_bower_tracking/tryPoseEstimation.py
+++ cichlid_bower_tracking/tryPoseEstimation.py
@@ -3,29 +3,39 @@ from helper_modules.file_manager import FileManager as FM
 from ultralytics import YOLO
 import torch
 
-"""
-# Check for MPS
-device = 'cpu' if torch.backends.mps.is_available() else 'cpu'
-print(f"Using device: {device}")
+def train_model(mode):
+	fm_obj = FM(analysisID = 'YH_MC_Parentals')
+
+	if mode == 'pose':
+
+		fm_obj.downloadData(fm_obj.localPoseDir + 'Benthics/')
+		model = YOLO("yolo26n-pose.pt")  # load a pretrained model (recommended for training)
+
+		results = model.train(data=fm_obj.localPoseDir + 'Benthics/data.yaml', epochs=100, imgsz=640, project = fm_obj.localMLPoseDir, name = 'Benthics', batch = -1, exist_ok=True)
+		fm_obj.uploadData(fm_obj.localMLPoseDir + 'Benthics/')
+
+	elif mode == 'detect':
+		fm_obj.downloadData(fm_obj.localYOLOAnnotationDir + 'GenericSex/')
+		model = YOLO("yolo26n.pt")  # load a pretrained model (recommended for training)
+		results = model.train(data=fm_obj.localYOLOAnnotationDir + 'GenericSex/data.yaml', epochs=100, imgsz=640, project = fm_obj.localYOLODir, name = 'GenericSexTest', batch = -1, exist_ok=True)
+		fm_obj.uploadData(fm_obj.localYOLODir + 'GenericSexTest/')
+
+def trackData():
+	# Check for MPS
+	device = 'cpu' if torch.backends.mps.is_available() else 'cpu'
+	print(f"Using device: {device}")
 
 
-fm_obj = FM(analysisID = 'YH_MC_Parentals')
-test_file = fm_obj.localLabeledDLCClipsDir + 'MCYHF1_549_t011_tr1__0009_vid__DLC.mp4'
-fm_obj.downloadData(test_file)
-fm_obj.downloadData(fm_obj.localYOLOModelDir)
-fm_obj.downloadData(fm_obj.localMLPoseDir + 'Benthics/weights/best.pt')
+	fm_obj = FM(analysisID = 'YH_MC_Parentals')
+	test_file = fm_obj.localLabeledDLCClipsDir + 'MCYHF1_549_t011_tr1__0009_vid__DLC.mp4'
+	fm_obj.downloadData(test_file)
+	fm_obj.downloadData(fm_obj.localYOLOModelDir)
+	fm_obj.downloadData(fm_obj.localMLPoseDir + 'Benthics/weights/last.pt')
 
-model_pose = YOLO(fm_obj.localMLPoseDir + 'Benthics/weights/best.pt')
-model_track = YOLO(fm_obj.localYOLOModelFile)
+	model_pose = YOLO(fm_obj.localMLPoseDir + 'Benthics/weights/last.pt')
+	model_track = YOLO(fm_obj.localYOLOModelFile)
 
-results = model_pose.track(test_file, show=True, device=device)
-"""
-# Train model
+	results = model_pose.track(test_file, show=True, device=device)
 
-fm_obj = FM(analysisID = 'YH_MC_Parentals')
-
-fm_obj.downloadData(fm_obj.localPoseDir + 'Benthics/')
-model = YOLO("yolo26x-pose.pt")  # load a pretrained model (recommended for training)
-
-results = model.train(data=fm_obj.localPoseDir + 'Benthics/data.yaml', epochs=100, imgsz=640, project = fm_obj.localMLPoseDir, name = 'Benthics', batch = -1, exist_ok=True)
-fm_obj.uploadData(fm_obj.localMLPoseDir + 'Benthics/')
+train_model('pose')
+train_model('detect')
