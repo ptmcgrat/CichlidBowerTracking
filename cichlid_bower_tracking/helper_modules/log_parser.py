a6d9ee41592d0ebc49d52fefa24c6fa787d087a0
Patrick McGrath <pmcgrath7@ipsec-10-2-97-178.vpn.gatech.edu>
2026-04-22 14:10:04 -0400

    Small updates

--- cichlid_bower_tracking/helper_modules/log_parser.py
+++ cichlid_bower_tracking/helper_modules/log_parser.py
@@ -100,7 +100,7 @@ class LogParser:
                 self.num_trials = len(self.tankresetstop) + 1
                 self.trials = [Trial(self.master_start, self.tankresetstart[0], self.tankresetstop[0], self.frames, self.movies, self.sampleID)]
                 for j in range(self.num_trials-2):
-                    self.trials.append(Trial(self.tankresetstop[j], self.tankresetstart[j+1], self.tankresetstop[j+1], self.frames, self.movie, self.sampleID))
+                    self.trials.append(Trial(self.tankresetstop[j], self.tankresetstart[j+1], self.tankresetstop[j+1], self.frames, self.movies, self.sampleID))
                 self.trials.append(Trial(self.tankresetstop[-1], end_time, None, self.frames, self.movies, self.sampleID))
             except IndexError:
                 self.trials = [Trial(self.master_start, end_time, None, self.frames, self.movies, self.sampleID)]
