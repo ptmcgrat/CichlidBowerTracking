fbd110b387ba7f66bfc7aec3f67d9555b737b3d8
Patrick McGrath <pmcgrath7@ipsec-10-2-224-85.vpn.gatech.edu>
2026-09-16 15:41:37 -0400

    Small updates

--- cichlid_bower_tracking/helper_modules/file_manager.py
+++ cichlid_bower_tracking/helper_modules/file_manager.py
@@ -498,8 +498,7 @@ class FileManager():
                 else:
                     pass
             else:
-                pdb.set_trace()
-                #raise FileNotFoundError('Cant find file for download: ' + cloud_path + relative_name)
+                raise FileNotFoundError('Cant find file for download: ' + cloud_path + relative_name)
         # pdb.set_trace()
         if not os.path.exists(local_path + relative_name):
             if allow_errors:
